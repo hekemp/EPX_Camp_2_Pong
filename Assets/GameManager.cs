33025ed3599c7003febc5e47370b23dc71be43eb
Heather Kemp <heather-kemp@uiowa.edu>
2019-01-15 23:44:24 -0600

    Set up basic project and prefabs/assets

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Ball ball;
+    public Paddle paddle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
